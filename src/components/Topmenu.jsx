b9b64d413209342aae5e82c17150c5d48d848679
Yumi_Kawai <yycharlottetown@gmail.com>
2024-04-19 11:47:18 -0300

    change css

--- src/components/Topmenu.jsx
+++ src/components/Topmenu.jsx
@@ -33,9 +33,9 @@ function Topmenu() {
       <main>
         <h2>Let&apos;s find the best cat food !</h2>
         <div className='main-menu'>
-            <button id="buttons" onClick={navigateToChat}>Ask Questions</button>
-            <button id="buttons" onClick={navigateToReport}>Summarize Reviews</button>
-            <button id="buttons" onClick={navigateToHistory}>View Existing Reviews</button>
+            <button id="main-buttons" onClick={navigateToChat}>Ask Questions</button>
+            <button id="main-buttons" onClick={navigateToReport}>Summarize Reviews</button>
+            <button id="main-buttons" onClick={navigateToHistory}>View Existing Reviews</button>
         </div>
       </main>
     </>
