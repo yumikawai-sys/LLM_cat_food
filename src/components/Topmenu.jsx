bc1350f97eadfab60c19768b7cb7b2d5ff5c1261
Yumi_Kawai <yycharlottetown@gmail.com>
2024-04-22 17:54:15 -0300

    add cat image

--- src/components/Topmenu.jsx
+++ src/components/Topmenu.jsx
@@ -33,9 +33,14 @@ function Topmenu() {
       <main>
         <h2>Let&apos;s find the best cat food !</h2>
         <div className='main-menu'>
-            <button id="main-buttons" onClick={navigateToChat}>Ask Questions</button>
-            <button id="main-buttons" onClick={navigateToReport}>Summarize Reviews</button>
-            <button id="main-buttons" onClick={navigateToHistory}>View Existing Reviews</button>
+            <div className="frame">
+              <img className="picture" src="cat2.jpg" alt="Cat Picture"/>
+            </div>
+            <div className='button-area'>
+              <button id="main-buttons" onClick={navigateToChat}>Ask Questions</button>
+              <button id="main-buttons" onClick={navigateToReport}>Summarize Reviews</button>
+              <button id="main-buttons" onClick={navigateToHistory}>View Existing Reviews</button>
+            </div>
         </div>
       </main>
     </>
